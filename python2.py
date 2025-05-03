37428767524abf0a511dd9b31aac12a275e1978d
GiftedGift <askhamegift@gmail.com>
2025-05-03 08:01:12 -0800

    task

---
+++ python2.py
@@ -0,0 +1,24 @@
+#a.this will display : x = 2
+x = 2
+y = 3
+
+print('x =', x)
+
+
+
+
+#b.this will display : 2 + 2 is 4
+print('value of', x , '+' , x, 'is', (x + x))
+
+
+
+
+#c.this will display : x =
+print('x =')
+
+
+
+
+
+#d.this will display : 5 = 5
+print((x + y), '=', (y + x))
