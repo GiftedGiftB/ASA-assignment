aa62876871ecb8e9654eb18d9bbd559e836620db
GiftedGift <askhamegift@gmail.com>
2025-05-10 17:03:48 -0800

    'task'

---
+++ chapter_3.8.py
@@ -0,0 +1,24 @@
+user_number1 = int(input('Enter first number: '))
+
+user_number2 = int(input('Enter second number: '))
+
+user_number3 = int(input('Enter third number: '))
+
+
+
+sum = (int(user_number1 + user_number2 + user_number3))
+
+
+average = (float(sum / 3))
+
+
+product = (int(user_number1 * user_number2 * user_number3))
+
+
+
+
+print('The sum is: ', sum)
+
+print('The average is: ', average)
+
+print('The product is: ', product)
---
+++ highest_score.py
@@ -0,0 +1,6 @@
+number_of_student = int(input('Enter number of student: '))
+while number_of_student in range(number_of_student):
+	number_of_student = int(input('Enter number of student: ')) 
+	name_of_student = input('Enter student name: ')
+	student_score = int(input('Enter student score: '))
+
