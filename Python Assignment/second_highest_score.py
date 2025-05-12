f3292e530ec069e72927a4a0d6e8cd7be5afb8d0
GiftedGift <askhamegift@gmail.com>
2025-05-11 23:01:29 -0800

    'task'

--- Python Assignment/second_highest_score.py
+++ Python Assignment/second_highest_score.py
@@ -6,13 +6,16 @@ secocnd_highest_student_score = 0
 
 for count in range(0,number_of_student):
 	name_of_student = input('Enter student name: ')
-	student_score = int(input('Enter student score: '))
+	student_score = (int(input('Enter student score: ')))
 
-if student_score > highest_student_score:
-	highest_student_name = name_of_student
-	highest_student_score = student_score
+	if student_score > highest_student_score:
+		highest_student_name = name_of_student
+		highest_student_score = student_score
+		second_highest_student_name = name_of_student
+		secocnd_highest_student_score = student_score
+	
+if secocnd_highest_student_score >= highest_student_score:
 	second_highest_student_name = name_of_student
 	secocnd_highest_student_score = student_score
-	
 
 print (f'The name of student with the highest score is {highest_student_name} with the highest score of {highest_student_score} and the second highest score goes to {second_highest_student_name} with the score of {secocnd_highest_student_score}')
