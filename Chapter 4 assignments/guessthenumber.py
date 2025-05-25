0fe0f896def4db15ad6f138229772e4baabf7d4d
GiftedGift <askhamegift@gmail.com>
2025-05-25 23:05:49 +0100

    still working on this

--- Chapter 4 assignments/guessthenumber.py
+++ Chapter 4 assignments/guessthenumber.py
@@ -1,2 +1,21 @@
+import random
+
 print("Lets play a guessing game")
-userNumber = int(input("guess the number: "))
+
+random_integer = random.randint(1, 1000 + 1)
+print(random_integer)
+
+break_out = int(input("Enter a number: "))
+
+user_guess_number = int(input("Guess a number between 1 - 1000: "))
+
+while break_out != -1:
+
+	if  random_integer == user_guess_number:
+		print("Congratulation. you win")
+	elif user_guess_number > random_integer:
+		print("Too high. Try again")
+	elif user_guess_number <  random_integer:
+		print("Too low. Try again")
+	else:
+		print("invalid guess")
