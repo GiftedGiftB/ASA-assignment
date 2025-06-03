53f76281b5011f191300528714dea8909df6c7a4
GiftedGiftB <askhamegift@gmail.com>
2025-06-03 23:48:01 +0100

    'today'

--- python_class_work/sumofdisplay.py
+++ python_class_work/sumofdisplay.py
@@ -1,6 +1,6 @@
 first_integer = int(input('Enter first number: '))
 
-second_integer = int(input('Enter second number number: '))
+second_integer = int(input('Enter second number: '))
 
 sum = first_integer + second_integer
 print('Their sum is: ', sum)
