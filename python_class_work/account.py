92db0d0ec1d736401d5742d287ca4204fe392b83
GiftedGiftB <asekhamegift@gmail.com>
2025-06-25 17:02:59 +0100

    class work on account

---
+++ python_class_work/account.py
@@ -0,0 +1,21 @@
+class Account:
+	#def __init__(self, name = "",  balance = 0):
+    def __init__(self):
+        #self.name = name
+        self.balance = 0
+
+    def withdraw(self):
+        print('Acount withdraw')
+
+    def deposit(self, amount):
+        #print('Account deposit')
+        self.balance += amount
+
+
+
+
+oba = Account()
+print(oba.balance)
+#oba.withdraw()
+oba.deposit(1000)
+print(oba.balance)
