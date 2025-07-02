d03fdb3980e328dcfbf81802885b56dcf5a5e325
GiftedGiftB <asekhamegift@gmail.com>
2025-07-02 23:59:07 +0100

    returned number of times each word appeared

---
+++ python_class_work/practicedictionary.py
@@ -0,0 +1,11 @@
+def return_a_dictionary_and_the_number_of_times_each_word_appeared(word):
+    words_to_be_checked = {}
+    for letter in word:
+        comprehension = {letter}
+        words_to_be_checked = comprehension
+        print(words_to_be_checked)
+
+
+
+
+return_a_dictionary_and_the_number_of_times_each_word_appeared("madamme")
