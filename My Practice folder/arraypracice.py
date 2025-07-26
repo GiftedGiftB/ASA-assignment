9e4e2604edf8c0893cad7cf9ac0f457a0d1b7848
GiftedGiftB <asekhamegift@gmail.com>
2025-07-26 03:18:27 +0100

    practicing array

--- My Practice folder/arraypracice.py
+++ My Practice folder/arraypracice.py
@@ -1,37 +1,83 @@
-number = [3,7,1,9,4]
-print(number[2])
+print()  # 1
+numbers = [3, 7, 1, 9, 4]
+print(numbers[2])
 
-fruit = ["apple", "banana", "cherry", "date"]
-print(fruit[-1])
+print()  #2
+list = ["apple", "banana", "cherry", "date"]
+print(list[-2])
 
-colors = ["red", "green", "blue", "yellow", "purple"]
-print(colors[1:4])
+print()  #3
+list = ["red", "green", "blue", "yellow", "purple"]
+print(list[1:4])
 
-animals = ["cat", "dog", "rabbit"]
-animals.append("elephant")
-print(animals)
+print()  #4
+list = ["cat", "dog", "rabbit"]
+list.append("elephant")
+print(list)
 
-numbers = [10, 20, 30, 40]
-for num in numbers:
-	print(num * 5)
+print()  #5
+list = [10, 20, 30, 40]
+for number in list:
+	print(number + 5)
 
-names = ["Ali", "Zara", "Mina"]
-for name in range(len(names)):
-	print(f"Hello, {names[name]}!")
+print()  #6
+list = ["Ali", "Zara", "Mina"]
+for name in range(len(list)):
+	print(f"Hello, {list[name]}")
 
-scores = [50, 60, 70, 80]
-scores[2] = 100
-print(scores)
+print()  #7
+list = [50, 60, 70, 80]
+list[2] = 100
+print(list)
 
-numbers = [5, 12, 7,20, 3]
-for num in numbers:
-	if num > 10:
-		print(num)
+print()  #8
+list = [5, 12, 7, 20, 3]
+for number in list:
 
-numbers = [2, 4, 6,8]
-total = 0
-for num in numbers:
-	total += num
-print(total)
+	if number > 10:
+		print(number)
 
+print()  #9
+list = [2, 4, 6, 8]
+result = 0
+for number in list:
+	result += number
+print(result)
 
+print()  #10
+list = ["Paris", "London", "Tokyo", "Dubai"]
+if "Paris" in list:
+	print("found")
+else:
+	print("not found")
+
+print()  #11
+temperatures = [22, 25, 19, 30, 28]
+print(max(temperatures))
+print()  # OR
+temperatures = [22, 25, 19, 30, 28]
+max_number = temperatures[0]
+
+for numbers in temperatures:
+	if numbers > max_number:
+		max_number = numbers
+print(max_number)
+
+print()  #12
+marks = [65, 70, 55, 80, 90]
+
+sum = 0
+count = 0
+for number in marks:
+	sum += number
+
+for number in marks:
+	count += 1
+average = sum / count
+
+print(average)
+
+print()  # 13
+letters = ['a', 'b', 'c', 'd', 'e']
+letters.pop(2)
+print(letters)
